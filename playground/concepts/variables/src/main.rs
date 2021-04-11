b0a519bbaf6847655fbf48a734062c4989c120c0
KeisukeShimokawa <shimoke4869@gmail.com>
2021-04-11 07:13:05 +0000

    docs: 問題7 タプルの宣言・取得方法を追加

--- playground/concepts/variables/src/main.rs
+++ playground/concepts/variables/src/main.rs
@@ -4,4 +4,12 @@ fn main() {
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    let tup: (i32, f64, u8) = (500, 0.1, 1);
+
+    let (a, b, c) = tup;
+
+    println!("The value of a, b, c is: {}, {}, {}", a, b, c);
+
+    println!("tup.0: {}, tup.1: {}, tup.2: {}", tup.0, tup.1, tup.2);
 }
