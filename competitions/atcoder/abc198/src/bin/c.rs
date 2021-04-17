d17b33e0b0508f3f0e0010081b8638d3c9f47aa5
KeisukeShimokawa <shimoke4869@gmail.com>
2021-04-17 23:41:37 +0900

    docs: C問題の解答を追加

--- competitions/atcoder/abc198/src/bin/c.rs
+++ competitions/atcoder/abc198/src/bin/c.rs
@@ -2,10 +2,16 @@ use proconio::input;
 
 fn main() {
     input! {
-        r: i32,
-        x: i32,
-        y: i32,
+        r: f64,
+        x: f64,
+        y: f64,
     }
 
-    let d1 = a;
+    let dist = (x * x + y * y).sqrt();
+
+    if dist < r {
+        println!("2");
+    } else {
+        println!("{}", (dist / r).ceil() as usize);
+    }
 }
