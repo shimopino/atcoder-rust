3a8840484e52f1a03e152677148d6e85d2d4e16a
KeisukeShimokawa <shimoke4869@gmail.com>
2021-04-27 01:06:54 +0900

    ABC150のB問題の解答を追加

---
+++ competitions/atcoder/abc150/src/bin/b.rs
@@ -0,0 +1,17 @@
+use proconio::input;
+use proconio::marker::Chars;
+
+fn main() {
+    input! {
+        n: u32,
+        s: Chars,
+    }
+
+    let mut count = 0;
+    for w in s.windows(3) {
+        if w[0] == 'A' && w[1] == 'B' && w[2] == 'C' {
+            count += 1;
+        }
+    }
+    println!("{}", count);
+}
