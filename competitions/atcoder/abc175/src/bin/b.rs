9b51d138cfce8a9d274b2db94858dae6f8c2b7b4
KeisukeShimokawa <shimoke4869@gmail.com>
2021-04-26 21:38:50 +0900

    ABC175のB問題を追加

---
+++ competitions/atcoder/abc175/src/bin/b.rs
@@ -0,0 +1,22 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        mut l: [i32; n],
+    }
+
+    l.sort();
+
+    let mut count = 0;
+    for i in 0..n {
+        for j in i+1..n {
+            for k in j+1..n {
+                if l[i] != l[j] && l[j] != l[k] && l[i] + l[j] > l[k] {
+                    count += 1;
+                }
+            }
+        }
+    }
+    println!("{}", count);
+}
