d384727bb8394ff409f3a2ca194198c5f20b1c6c
KeisukeShimokawa <shimoke4869@gmail.com>
2021-05-02 12:46:05 +0900

    feat: ABC079のB問題の動的計画法での解答を追加

--- competitions/atcoder/abc079/src/bin/b_r1.rs
+++ competitions/atcoder/abc079/src/bin/b_r1.rs
@@ -2,12 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: u32,
+        n: usize
     }
 
-    let mut ans = 1;
-    while ans * 2 <= n {
-        ans *= 2;
+    let mut dp = vec![0u64; n + 1];
+    dp[0] = 2;
+    dp[1] = 1;
+    for i in 2..=n {
+        dp[i] = dp[i - 1] + dp[i - 2];
     }
-    println!("{}", ans);
+    println!("{}", dp[n]);
 }
