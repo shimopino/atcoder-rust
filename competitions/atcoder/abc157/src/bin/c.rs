7f1c46c52e7d69211020e46d921380e752e101d8
KeisukeShimokawa <shimoke4869@gmail.com>
2021-05-05 05:11:27 +0900

    ABC157のC問題でコンパイルエラーを修正

--- competitions/atcoder/abc157/src/bin/c.rs
+++ competitions/atcoder/abc157/src/bin/c.rs
@@ -2,8 +2,8 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        m: usize,
+        n: u32,
+        m: u32,
         pair: [(usize, char); m],
     }
 
@@ -11,8 +11,8 @@ fn main() {
     for target in 0..10i32.pow(n) {
         let digit: Vec<char> = target.to_string().chars().collect();
 
-        if digit.len() == n {
-            if pair.iter().all(|(s, c| matches!(digit.get(*s-1), Some(x) if *x == *c))) {
+        if digit.len() == n as usize {
+            if pair.iter().all(|(s, c)| matches!(digit.get(*s-1), Some(x) if *x == *c)) {
                 ans = target;
                 break;
             }
