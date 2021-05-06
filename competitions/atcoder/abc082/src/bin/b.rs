b0c2267177c3c8fa123143320a0be667b8b8dd9b
KeisukeShimokawa <shimoke4869@gmail.com>
2021-05-06 15:04:55 +0900

    ABC082のB問題で文字列のソートと逆順解答を追加

---
+++ competitions/atcoder/abc082/src/bin/b.rs
@@ -0,0 +1,14 @@
+use proconio::input;
+use proconio::marker::Chars;
+
+fn main() {
+    input! {
+        mut s: Chars,
+        mut t: Chars,
+    }
+
+    s.sort();
+    t.sort();
+    t.reverse();
+    println!("{}", if s < t { "Yes" } else { "No" });
+}
