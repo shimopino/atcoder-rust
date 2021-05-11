09a4ac33ec00da4c890220156d8a035b49d20a2e
KeisukeShimokawa <shimoke4869@gmail.com>
2021-05-11 13:15:44 +0900

    ABC071のA問題で絶対値計算の解答を追加

---
+++ competitions/atcoder/abc071/src/bin/a.rs
@@ -0,0 +1,15 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        x: i32,
+        a: i32,
+        b: i32,
+    }
+
+    if (x - a).abs() < (x - b).abs() {
+        println!("A");
+    } else {
+        println!("B");
+    }
+}
