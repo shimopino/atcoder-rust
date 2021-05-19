54c12ac54ef70955630b70993c378a88330c1014
KeisukeShimokawa <shimoke4869@gmail.com>
2021-05-19 22:51:37 +0900

    ABC197のC問題でBit全探索の解答を追加

--- competitions/atcoder/abc197/src/bin/c.rs
+++ competitions/atcoder/abc197/src/bin/c.rs
@@ -3,16 +3,22 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        a: [usize; n],
+        a: [i32; n],
     }
 
-    let a_bin: Vec<String> = a.iter()
-                              .map(|i| format!("{:0>30b}", i))
-                              .collect();
+    let mut ans = std::i32::MAX;
+    for bit in 0..(1 << (n - 1)) {
+        let mut or = 0;
+        let mut xor = 0;
 
-    for i in a_bin.iter() {
-        println!("{}", i);
+        for (i, &a) in a.iter().enumerate() {
+            or |= a;
+            if bit & (1 << i) == 0b0 {
+                xor ^= or;
+                or = 0;
+            }
+        }
+        ans = ans.min(xor ^ or);
     }
-
-    println!("{}", 3 & 5);
+    println!("{}", ans);
 }
